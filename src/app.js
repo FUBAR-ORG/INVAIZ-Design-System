46b12198445d54eb125a4dedf7a029921063befb
SewookHan <hkihki306@gmail.com>
2021-10-09 20:28:20 +0900

    Setup react project

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h2>Hello World</h2>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
