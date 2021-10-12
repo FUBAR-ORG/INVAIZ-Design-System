480d90ed2a73cac6f1768bd9b3bb3faac261e154
HelloMandu <tjdals6695@gmail.com>
2021-10-12 21:23:12 +0900

    webpack config 일부 수정, css 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 
 export default function App() {
   return <h2>Hello World</h2>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const mode = process.env.NODE_ENV || "development"
 
 module.exports = {
-  mode: 'development',
+  mode,
+  entry: {
+    app: './src'
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js'
+  },
   devServer: {
-    port: 8000,
+    port: 8080,
+    open: true,
+    client: {
+      overlay: true
+    },
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(js|jsx)?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -23,6 +43,15 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+      },
+      minify: process.env.NODE_ENV === 'production' ? {
+        collapseWhitespace: true,
+        removeComments: true
+      } : false,
     }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
   ],
 };
