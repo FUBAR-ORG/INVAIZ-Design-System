3334d832230a765b3103537b7479e26e313713ce
Sewook Han <hkihki306@gmail.com>
2021-10-13 20:13:26 +0900

    storybook setting (#9)

---
+++ src/stories/Example.tsx
@@ -0,0 +1,18 @@
+export interface ComponentProps {
+  text: string;
+}
+
+export default function Component({text}:ComponentProps) {
+  return (
+    <div style={{
+      backgroundColor:'yellow',
+      width:'200px',
+      height:'70px',
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center'
+    }}>
+      <h3>{text}</h3>
+    </div>
+  )
+}
