7f5f53b065d1557e56d08e2aab0e8b155e578b30
HelloMandu <tjdals6695@gmail.com>
2021-11-07 22:08:11 +0900

    folder 추가

--- src/test/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('test', () => {
-  const { getByTestId } = render(<App />);
-  const testId = getByTestId('test-id');
-  expect(testId.innerHTML).toMatch('Project init');
-});
