39cf8de840590b79b422b21050715bd9c310ef2f
Sungmin Jo <tjdals6695@gmail.com>
2022-01-02 16:46:19 +0900

    antd icon package install, Jest, Storybook GlobalTheme setup (#28)
    * ant design icon package install
    
    * - jest moduleNameMapper fix
    - Button default testcode 작성
    
    * yarn.lock fix
    
    * storybook GlobalThemeProvider.tsx 적용
    
    * folder fix
    
    * - test alias
    - custom render
    
    * import react remove

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyle from '@theme/GlobalStyle';
 import GlobalThemeProvider from '@theme/GlobalThemeProvider';
 import App from './App';
 
@@ -8,7 +7,6 @@ ReactDOM.render(
   <React.StrictMode>
     <GlobalThemeProvider>
       <App />
-      <GlobalStyle />
     </GlobalThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/theme/GlobalThemeProvider.tsx
+++ src/theme/GlobalThemeProvider.tsx
@@ -2,9 +2,15 @@ import { PropsWithChildren } from 'react';
 import { Theme, ThemeProvider } from '@emotion/react';
 import font, { fontSize, fontWeight, lineHeight } from '@theme/font';
 import color from '@theme/color';
+import GlobalStyle from '@theme/GlobalStyle';
 
 export default function GlobalThemeProvider({ children }: PropsWithChildren<unknown>) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 const theme: Theme = { font, fontSize, lineHeight, fontWeight, color };
