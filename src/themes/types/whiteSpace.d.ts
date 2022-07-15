362de4f754598354a124346698728646a93c23a6
changjunPark <ckdwns9121@naver.com>
2022-07-15 17:30:17 +0900

    # feat: 여백 타입 작성

---
+++ src/themes/types/whiteSpace.d.ts
@@ -0,0 +1,27 @@
+
+
+export interface IWhiteSpace{
+    /** 4px. Micro Size 입니다. */
+    whiteSpace4 : number;
+    /** 10px. Small Size 입니다. */
+    
+    whiteSpace10 : number;
+    /** 20px. Middle Size 입니다. */
+
+    whiteSpace20: number;
+    /** 24px. Basic Size 입니다. */
+
+   whiteSpace24: number;
+    /** 32px. Basic+ Size 입니다. */
+
+   whiteSpace32: number;
+    /** 40px. Large Size 입니다. */
+
+   whiteSpace40:number;
+    /** 48px. Large+ Size 입니다. */
+
+   whiteSpace48:number;
+    /** 72px. X-Large Size 입니다. */
+
+   whiteSpace72:number;
+}
