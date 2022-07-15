2595d990c70790c51f752e9a921b03451f929875
changjunPark <ckdwns9121@naver.com>
2022-07-15 17:35:14 +0900

    # feat: 여백 템플릿 작성

---
+++ src/themes/whiteSpace.ts
@@ -0,0 +1,14 @@
+import type { IWhiteSpace } from '@themes/types/whiteSpace';
+
+const whiteSpace : IWhiteSpace ={
+    whiteSpace4 : 4,
+    whiteSpace10 : 10,
+    whiteSpace20 : 20,
+    whiteSpace24 : 24,
+    whiteSpace32 : 32,
+    whiteSpace40 : 40,
+    whiteSpace48 : 48,
+    whiteSpace72 : 72,
+} 
+
+export default whiteSpace;
