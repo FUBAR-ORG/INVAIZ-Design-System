9045cee7adbc4a470cd016c60855219b7271ea53
HelloMandu <tjdals6695@gmail.com>
2022-07-24 20:22:30 +0900

    storybook: storybook vite 버전으로 변경 v2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config) => {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal(config) {
     config.resolve.alias['@components'] = path.resolve(__dirname, '../src/components');
     config.resolve.alias['@themes'] = path.resolve(__dirname, '../src/themes');
     config.resolve.alias['@assets'] = path.resolve(__dirname, '../src/assets');
     config.resolve.alias['@tests'] = path.resolve(__dirname, '../src/tests');
     config.resolve.alias['@emotion/core'] = path.join(process.cwd(), 'node_modules/@emotion/react');
     return config;
-  },
-};
+  }
+}
