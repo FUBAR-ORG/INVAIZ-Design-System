90d5e881a77ae536e1215389bc7b7abfb1a8c07b
HelloMandu <tjdals6695@gmail.com>
2022-07-24 18:33:19 +0900

    svg: vite-plugin-svgr install

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import styled from '@emotion/styled';
+import { ReactComponent as Add } from '@assets/svg/Add.svg';
 
 export default function App() {
-  return <ThemeTest>Project Init</ThemeTest>;
+  return <Add />;
 }
 
-const ThemeTest = styled.div`
-  color: ${({ theme }) => theme.color.light};
-  ${({ theme }) => theme.font.kopub.title1};
-  ${({ theme }) => theme.style.fontFace.kopub}
-`;
+// const ThemeTest = styled.div`
+//   color: ${({ theme }) => theme.color.light};
+//   ${({ theme }) => theme.font.kopub.title1};
+//   ${({ theme }) => theme.style.fontFace.kopub}
+// `;
