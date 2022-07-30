c76c8a2032ee221f0c6eace72eb631c454d32702
HelloMandu <tjdals6695@gmail.com>
2022-07-31 02:04:01 +0900

    fix: 리뷰 반영

--- src/components/Toggles/Toggle.tsx
+++ src/components/Toggles/Toggle.tsx
@@ -8,8 +8,8 @@ export interface ToggleProps {
 function Toggle({ checked, onChange }: ToggleProps) {
   return (
     <Label>
-      <input type='checkbox' checked={checked} onChange={(e) => onChange?.(e.target.checked)} />
-      <span />
+      <Input type='checkbox' checked={checked} onChange={(e) => onChange?.(e.target.checked)} />
+      <Span />
     </Label>
   );
 }
@@ -19,38 +19,40 @@ const Label = styled.label`
   display: inline-block;
   width: 48px;
   height: 24px;
-  input {
-    width: 0;
-    height: 0;
-    opacity: 0;
-  }
-  span {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: ${({ theme }) => theme.color.light};
-    border-radius: 12px;
-    transition: all 0.15s;
-    &:before {
-      position: absolute;
-      content: '';
-      width: 20px;
-      height: 20px;
-      top: 2px;
-      left: 2px;
-      background: ${({ theme }) => theme.darkColor.basic};
-      border-radius: 100%;
-      transition: all 0.15s;
-    }
-  }
   input:checked + span {
     background: ${({ theme }) => theme.color.point1};
   }
   input:checked + span:before {
     transform: translateX(24px);
+  }
+`;
+
+const Input = styled.input`
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+const Span = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: ${({ theme }) => theme.color.light};
+  border-radius: 12px;
+  transition: all 0.15s;
+  &:before {
+    position: absolute;
+    content: '';
+    width: 20px;
+    height: 20px;
+    top: 2px;
+    left: 2px;
+    background: ${({ theme }) => theme.darkColor.basic};
+    border-radius: 100%;
+    transition: all 0.15s;
   }
 `;
 
