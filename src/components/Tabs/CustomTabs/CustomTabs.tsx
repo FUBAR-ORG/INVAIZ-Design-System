e9e7788c82b848c86d1b14e740318656b8371a10
Sewook Han <hkihki306@gmail.com>
2022-08-14 10:07:34 +0900

    # fix: update interface

--- src/components/Tabs/CustomTabs/CustomTabs.tsx
+++ src/components/Tabs/CustomTabs/CustomTabs.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styled from '@emotion/styled';
 import CustomTab from './CustomTab';
 import { Props as TabsProps } from '../Tabs';
@@ -7,18 +6,11 @@ import { Props as TabsProps } from '../Tabs';
 사용자가 커스텀할 수 있는 콘텐츠일 경우 사용하는 탭 메뉴입니다. 
 선택된 콘텐츠는 강조되고, 선택되지 않은 콘텐츠는 연하게 처리됩니다.
  */
-export default function CustomTabs({ tabs }: TabsProps) {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+export default function CustomTabs({ selectedIndex, tabs, onClick }: TabsProps) {
   return (
     <Wrapper>
       {tabs.map((tab, i) => (
-        <CustomTab
-          type='button'
-          active={i === activeIndex}
-          onClick={() => setActiveIndex(i)}
-          key={tab}
-        >
+        <CustomTab type='button' active={i === selectedIndex} onClick={() => onClick(i)} key={tab}>
           {tab}
         </CustomTab>
       ))}
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,14 +5,15 @@ import Underline from './Underline';
 
 export interface Props {
   tabs: string[];
+  selectedIndex?: number;
+  onClick(selectedIndex: number): void;
 }
 
 /**
  사용자가 커스텀 할 수 없는 제작자가 제공하는 콘텐츠일 경우 사용하는 탭 메뉴 입니다.
  하단바의 막대가 좌우로 움직이며 인터랙션합니다.
  */
-export default function Tabs({ tabs }: Props) {
-  const [activeIndex, setActiveIndex] = useState(0);
+export default function Tabs({ tabs, selectedIndex = 0, onClick }: Props) {
   const [underlinePos, setUnderlinePos] = useState(0);
   const [underlineWidth, setUnderlineWidth] = useState(0);
 
@@ -20,7 +21,7 @@ export default function Tabs({ tabs }: Props) {
 
   useEffect(() => {
     const setTabUnderline = () => {
-      const currentTab = tabsRef.current[activeIndex];
+      const currentTab = tabsRef.current[selectedIndex];
       setUnderlinePos(currentTab?.offsetLeft ?? 0);
       setUnderlineWidth(currentTab?.clientWidth ?? 0);
     };
@@ -30,7 +31,7 @@ export default function Tabs({ tabs }: Props) {
     window.addEventListener('resize', setTabUnderline);
 
     return () => window.removeEventListener('resize', setTabUnderline);
-  }, [activeIndex]);
+  }, [selectedIndex]);
 
   return (
     <Wrapper>
@@ -41,7 +42,7 @@ export default function Tabs({ tabs }: Props) {
           ref={(el: HTMLButtonElement | null) => {
             tabsRef.current[i] = el;
           }}
-          onClick={() => setActiveIndex(i)}
+          onClick={() => onClick(i)}
         >
           {tab}
         </Tab>
