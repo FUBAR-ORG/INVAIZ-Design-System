7ae3d634e46a90cf5d4895e3a7d7bf1d853abd63
IRONAGE-Park <cheolhyundev@gmail.com>
2022-08-27 02:30:26 +0900

    # add: `Subtract` 아이콘 추가 및 아이콘 컬러 적용

--- src/components/SvgIcons/SvgIcon.tsx
+++ src/components/SvgIcons/SvgIcon.tsx
@@ -1,12 +1,23 @@
 import { SVGProps } from 'react';
+import styled from '@emotion/styled';
 import SVG_ICONS from '@assets/svg';
 
 export interface SvgIconProps extends SVGProps<SVGSVGElement> {
+  /** 아이콘의 형태 */
   icon: keyof typeof SVG_ICONS;
+  /** 아이콘 사이즈 */
   size?: 16 | 20 | 24;
+  /** 아이콘에 적용될 컬러 */
+  color?: string;
 }
 
-export default function SvgIcon({ icon, size = 24, ...props }: SvgIconProps) {
+const NoneStyleSvgIcon = ({ icon, size = 24, color, ...props }: SvgIconProps) => {
   const Icon = SVG_ICONS[icon];
-  return <Icon width={size} height={size} {...props} />;
-}
+  return <Icon width={size} height={size} style={{ fill: color }} {...props} />;
+};
+
+const SvgIcon = styled(NoneStyleSvgIcon)`
+  fill: ${({ theme }) => theme.color.grayScale.basic.black};
+`;
+
+export default SvgIcon;
