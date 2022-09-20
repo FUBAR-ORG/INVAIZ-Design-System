583ba9ea263e0ac3e81e9aac0a1828812d8ae0fa
changjun <ckdwns9121@naver.com>
2022-09-21 00:03:01 +0900

    # text button style 작업

--- src/components/Buttons/TextButton.tsx
+++ src/components/Buttons/TextButton.tsx
@@ -23,7 +23,31 @@ export default function TextButton({
 }
 
 const Text = styled.button`
+  //default style
+  min-width: 80px;
+  min-height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   cursor: pointer;
   outline: none;
   border: none;
+  transition: all 0.15s ease-in-out;
+  border-radius: 5px;
+
+  // space
+  padding: ${({ theme }) => theme.whiteSpace.whiteSpace4}px;
+  box-sizing: border-box;
+
+  // font
+  font-size: ${({ theme }) => theme.fontSize.size14}px;
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+
+  // color
+  background-color: transparent;
+  color: ${({ theme }) => theme.color.grayScale.gray900};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.grayScale.coolGray100};
+  }
 `;
