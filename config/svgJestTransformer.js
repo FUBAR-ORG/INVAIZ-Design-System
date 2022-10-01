70c964db97d437d183c246ad2fd922527f8a1a07
HelloMandu <tjdals6695@gmail.com>
2022-10-01 22:32:23 +0900

    Dropdown Test 작성

---
+++ config/svgJestTransformer.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+function buildModule(functionName, pathname, filename) {
+  return {
+    code: `
+      const React = require('react');
+      
+      const ${functionName} = (props) => {
+        return React.createElement('svg', { 
+          ...props, 
+          'data-file-name': '${pathname}',
+          'data-svg-name': '${filename}',
+          'data-test-id': '${functionName}'
+        });
+      }
+      module.exports.default = ${functionName};
+      module.exports.ReactComponent = ${functionName};
+    `,
+  };
+}
+
+function createFunctionName(base) {
+  const words = base.split(/\W+/);
+  return words.reduce(
+    (identifer, word) => identifer + word.substr(0, 1).toUpperCase() + word.substr(1),
+    ''
+  );
+}
+
+function processSvg(contents, filename) {
+  const parts = path.parse(filename);
+  if (parts.ext.toLowerCase() === '.svg') {
+    const functionName = createFunctionName(parts.name);
+    return buildModule(functionName, parts.base, parts.name);
+  }
+
+  return contents;
+}
+
+module.exports = {
+  process: processSvg,
+};
