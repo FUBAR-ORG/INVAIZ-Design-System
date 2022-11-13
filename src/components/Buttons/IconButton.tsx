62bafa6e88f1757d050bc45af00c44f4fbdc44fa
ckdwns9121 <ckdwns9121@naver.com>
2022-11-13 16:24:54 +0900

    # feat: 아이콘버튼 타입 수정

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -1,18 +1,25 @@
 import styled from '@emotion/styled';
 import SvgIcon, { SvgIconProps } from '@components/SvgIcons/SvgIcon';
 import { ButtonHTMLAttributes } from 'react';
-import SVG_ICONS from '@assets/svg';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   shape?: 'default' | 'pill';
 }
 
-export type IconButtonProps = ButtonProps & SvgIconProps;
+export type IconButtonProps = ButtonProps & Pick<SvgIconProps, 'icon' | 'size' | 'color'>;
 
-export default function IconButton({ icon, size = 24, color = '#fff', ...props }: IconButtonProps) {
+export default function IconButton({
+  icon,
+  size = 24,
+  color = '#fff',
+  shape = 'default',
+  ...props
+}: IconButtonProps) {
+  const cn = shape === 'pill' ? 'pill' : '';
+
   return (
-    <StyledIconButton>
-      <SvgIcon icon={icon} size={size} color={color} {...props} />
+    <StyledIconButton {...props} className={[cn, props.className].join(' ')}>
+      <SvgIcon icon={icon} size={size} color={color} />
     </StyledIconButton>
   );
 }
@@ -31,4 +38,8 @@ const StyledIconButton = styled.button`
   box-sizing: border-box;
   border-radius: 5px;
   background-color: ${({ theme }) => theme.color.primary.blue500};
+
+  &.pill {
+    border-radius: 100%;
+  }
 `;
