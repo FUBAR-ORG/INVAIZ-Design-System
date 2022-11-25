ec2f91125588644851091105ab521a9f07445533
jeonghyun <k557330@naver.com>
2022-11-25 12:03:25 +0900

    # REMOVE: Clearable icon in test [complete]

--- src/components/Inputs/Input.test.tsx
+++ src/components/Inputs/Input.test.tsx
@@ -6,23 +6,19 @@ describe('Input', () => {
   const VALUE = 'filled';
 
   it('clearable이 true일 때 value에 값이 없으면 내용을 지우는 버튼이 렌더링되지 않는다.', () => {
-    const { queryByRole, queryByTestId } = render(<Input value='' clearable />);
+    const { queryByRole } = render(<Input value='' clearable />);
 
     const button = queryByRole('button');
-    const clearIcon = queryByTestId('clear-icon');
 
     expect(button).toBeNull();
-    expect(clearIcon).toBeNull();
   });
 
   it('clearable이 true일 때 value에 값이 있으면 내용을 지우는 버튼이 렌더링된다.', () => {
-    const { getByRole, getByTestId } = render(<Input value={VALUE} clearable />);
+    const { getByRole } = render(<Input value={VALUE} clearable />);
 
     const button = getByRole('button');
-    const clearIcon = getByTestId('clear-icon');
 
     expect(button).toBeInTheDocument();
-    expect(clearIcon).toBeInTheDocument();
   });
 
   it('required가 true일 때 value에 값이 없으면 required icon이 렌더링된다.', () => {
